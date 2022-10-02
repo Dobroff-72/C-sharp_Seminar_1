5a12645e6699679fd16cf0a4d53f5805d3d881c6
dobrov.mn <dobroff-72@inbox.ru>
2022-10-02 23:00:43 +0500

    Add task2_MaxNum

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(a);
+}
+if(a < b)
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(b);
+}
+if(a == b)
+{
+    Console.Write("Числа равны");
+}
