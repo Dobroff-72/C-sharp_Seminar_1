6ba8395c164f180c9e53bf64862112acf283af77
dobrov.mn <dobroff-72@inbox.ru>
2022-10-03 19:46:29 +0500

    Add task_6

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+    Console.Write("Число нечётное");
+}
