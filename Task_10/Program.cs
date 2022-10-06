40183ee94ae32cf524ec6b05c704145e22f7ffcf
dobrov.mn <dobroff-72@inbox.ru>
2022-10-06 21:40:45 +0500

    Add Task10

---
+++ Task_10/Program.cs
@@ -0,0 +1,26 @@
+﻿ //Сложно, длинно, но работает oO)
+ 
+ 
+ int SecondNum(int n)
+{
+ int result = n / 10 % 10;
+ if(result < 0)
+ {
+    result *= -1;                 //Если число отрицательное, то перевод в положительное
+ }
+ return result;
+}
+
+
+ Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n > -100 & n < 100 | n > 1000 | n < -1000 )
+{
+    Console.WriteLine("Введите трехзначное число!!!");
+    int a = Convert.ToInt32(Console.ReadLine());
+    n = a;                                            //Пока что не понял, как сделать без дополнительной переменной.
+
+}
+
+int res = SecondNum(n);
+Console.WriteLine(res);
