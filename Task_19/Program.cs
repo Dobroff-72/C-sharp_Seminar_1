6305aca5f7e3439167b5f6e5b0d6b2f9205e08bb
dobrov.mn <dobroff-72@inbox.ru>
2022-10-12 21:43:42 +0500

    проверяет, является ли число палиндромом

---
+++ Task_19/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+num =Math.Abs(num);
+int OldNum = num;
+int newNum = 0;
+while(num > 0)
+{
+    int i = num % 10;
+    newNum = newNum * 10 + i;
+    num = num / 10;
+}
+if(newNum == OldNum)
+    {
+      Console.WriteLine("Данное число является палиндромом");  
+    }
+else
+{
+    Console.WriteLine("Нет, это не палиндром");
+}
