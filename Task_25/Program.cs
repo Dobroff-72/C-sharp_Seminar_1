144cb23fceecc73838e466128e85c14b4b5e2489
dobrov.mn <dobroff-72@inbox.ru>
2022-10-13 21:39:50 +0500

    принимает на вход два числа (A и B) и возводит число A в натуральную степень B

---
+++ Task_25/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+Console.WriteLine("Введите число А: ");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите cтепень B: ");
+int N2 = int.Parse(Console.ReadLine());
+Degree(N1, N2);
+
+
+
+void Degree(int N1, int N2)
+{
+    double result = Math.Pow(N1, N2);
+    Console.Write("Число А в степени В равно: " + result ); 
+}
+
