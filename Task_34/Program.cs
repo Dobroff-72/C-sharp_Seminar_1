d25eb0f93eb6f994f10f8892b301e759b0c660e9
dobrov.mn <dobroff-72@inbox.ru>
2022-10-17 20:17:30 +0500

    Напишите программу, которая покажет количество чётных чисел в массиве

---
+++ Task_34/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] fillMass(int[] array, int from, int to)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(from, to + 1);
+    }
+    return array;
+}
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void findNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count ++;
+        }
+    }
+    Console.WriteLine("Количество четных чисел в массиве: " + count);
+}
+
+int[] array = new Int32[10]; // Массив из 10 чисел
+fillMass(array,100,999);  // от 100 до 999
+printArray(array);
+findNum(array);
