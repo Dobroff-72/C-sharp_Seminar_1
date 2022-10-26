4e75b0759cbc9f7268f21fc2a9d540de824f5174
dobrov.mn <dobroff-72@inbox.ru>
2022-10-26 21:29:00 +0500

    add ; for result

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -32,7 +32,7 @@ void Print(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(array[i,j] + " ");
+            Console.Write(array[i,j] + "; ");
         }
         Console.WriteLine();
     }
